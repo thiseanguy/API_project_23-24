5cb9866aabc81b33f35e58d7943cb1c4e8c20db8
Sean Hatcher <thiseanguy@gmail.com>
2024-04-17 21:55:13 -0500

    api router test successful, phase-1 complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,10 @@
+// backend/routes/api/index.js
 const router = require('express').Router();
 
 //test route
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
+
 
 module.exports = router;
