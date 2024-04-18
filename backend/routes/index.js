1c6f5d472b4c2ed8e807f558c3a2eda04e68adba
Sean Hatcher <thiseanguy@gmail.com>
2024-04-17 21:55:13 -0500

    api router test successful, phase-1 complete

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,15 +1,10 @@
-
+// backend/routes/index.js
 const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-// // test route
-// router.get('/hello/world', function(req, res) {
-//   res.cookie('XSRF-TOKEN', req.csrfToken());
-//   res.send('Hello World!');
-// });
 
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
@@ -19,7 +14,6 @@ router.get("/api/csrf/restore", (req, res) => {
       'XSRF-Token': csrfToken
     });
   });
-
 
 
 module.exports = router;
