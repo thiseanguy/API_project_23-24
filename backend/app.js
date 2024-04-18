8169032f7a247ee621ade93dcf89d7dfc8fb50e7
Sean Hatcher <thiseanguy@gmail.com>
2024-04-17 22:12:53 -0500

    error formatter/handler added to end of app.js file, phase2 complete -SH

--- backend/app.js
+++ backend/app.js
@@ -72,4 +72,16 @@ app.use((err, _req, _res, next) => {
   next(err);
 });
 
+// Error formatter (should be the last middleware in the app.js file)
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
+
   module.exports = app;
