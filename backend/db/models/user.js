2c02c5a9e043fee5a71564bb4242b5c625dc0feb
Sean Hatcher <thiseanguy@gmail.com>
2024-04-18 20:24:01 -0500

    bcrypt was installed from wrong folder, files deleted and reinstalled properly, starting Model Scopes

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -41,6 +41,12 @@ module.exports = (sequelize, DataTypes) => {
     }, {
     sequelize,
     modelName: 'User',
-  });
+    defaultScope: {
+      attributes: {
+        exclude: ["hashedPassword", "email", "createdAt", "updatedAt"]
+      }
+    }
+  }
+);
   return User;
 };
