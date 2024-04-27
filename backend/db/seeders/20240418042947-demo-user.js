5c90abefda0f53776704d896541fe35ba604a473
Sean Hatcher <thiseanguy@gmail.com>
2024-04-27 15:25:51 -0500

    added firstname and lastname to each user in seeder file

--- backend/db/seeders/20240418042947-demo-user.js
+++ backend/db/seeders/20240418042947-demo-user.js
@@ -15,16 +15,22 @@ module.exports = {
     options.tableName = "Users";
     await User.bulkCreate(options, [
       {
+        firstName: 'Demo',
+        lastName: 'Lition',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: 'John',
+        lastName: 'Smith',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: 'Fake',
+        lastName: 'User',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
