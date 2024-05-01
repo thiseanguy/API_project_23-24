afe878962c521d29c65afbdf7a89a99fe63e555b
Sean Hatcher <thiseanguy@gmail.com>
2024-04-30 22:13:11 -0500

    added get spot by current user & get spot by user id endpoints

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -3,12 +3,18 @@ const express = require('express')
 const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
 
+const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
+
 const { User } = require('../../db/models');
 const { Spot } = require('../../db/models');
 
+//const { check } = require('express-validator');
+//const { handleValidationErrors } = require('../../utils/validation');
+
 const router = express.Router();
 
-
+//restores the user object from the JWT toke
+router.use(restoreUser);
 
 
 // Get all spots
@@ -44,14 +50,19 @@ try {
 }
 });
 
-// Get all spots owned by the Current User
-router.get('/current', async (req, res) => {
+
+
+router.get('/current', requireAuth, async (req, res) => {
     try {
+        const userId = req.user.id;
+        //const userId = req.user.id;
+
         const spots = await Spot.findAll({
             where: {
-                ownerId: currUser
+                ownerId: userId
             }
         });
+
 
         const resBody = {
             Spots: spots.map(spot => ({
@@ -73,6 +84,7 @@ router.get('/current', async (req, res) => {
             }))
         };
 
+
         res.status(200);
         res.json(resBody);
     } catch (error) {
@@ -81,6 +93,42 @@ router.get('/current', async (req, res) => {
     }
     });
 
+    //Get spot details by id
+    router.get('/:spotId', async (req, res) => {
+        try {
+            const spotId = req.params.spotId;
+
+            const spot = await Spot.findByPk(spotId);
+
+            if (!spot) {
+                return res.status(404).json({ error: 'Spot not found' });
+            }
+
+            const resBody = {
+                id: spot.id,
+                ownerId: spot.ownerId,
+                address: spot.address,
+                city: spot.city,
+                state: spot.state,
+                country: spot.country,
+                lat: spot.lat,
+                lng: spot.lng,
+                name: spot.name,
+                description: spot.description,
+                price: spot.price,
+                createdAt: spot.createdAt,
+                updatedAt: spot.updatedAt,
+                avgRating: spot.avgRating,
+                previewImage: spot.previewImage
+            };
+
+            res.status(200)
+            res.json(resBody);
+        } catch (error) {
+            console.error('Error fetching spot:', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    });
 
 
 
