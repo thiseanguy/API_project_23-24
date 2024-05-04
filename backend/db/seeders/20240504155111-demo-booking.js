3e66f59818b1827ed84286cf73744b8b2bf4344e
Sean Hatcher <thiseanguy@gmail.com>
2024-05-04 11:55:41 -0500

    fixed error in booking seeder

--- backend/db/seeders/20240504155111-demo-booking.js
+++ backend/db/seeders/20240504155111-demo-booking.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === 'production') {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await Review.bulkCreate([
+    await Booking.bulkCreate([
       {
         id: 1,
         spotId: 1,
