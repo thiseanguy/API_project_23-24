599ad56c9450631c8421935242fcbfdbd8a4f22b
Sean Hatcher <thiseanguy@gmail.com>
2024-06-04 20:52:42 -0500

    added dropdown navigation menu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,10 @@ import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage/LoginFormPage';
+import SignupFormPage from './components/SignupFormPage/SignupFormPage';
+import Navigation from './components/Navigation/Navigation';
 import * as sessionActions from './store/session';
+import './App.css'
 
 function Layout() {
   const dispatch = useDispatch();
@@ -16,10 +19,12 @@ function Layout() {
 
   return (
     <>
+      <Navigation isLoaded={isLoaded} />
       {isLoaded && <Outlet />}
     </>
   );
 }
+
 
 const router = createBrowserRouter([
   {
@@ -32,6 +37,10 @@ const router = createBrowserRouter([
       {
         path: '/login',
         element: <LoginFormPage />
+      },
+      {
+        path: "/signup",
+        element: <SignupFormPage />
       }
     ]
   }
