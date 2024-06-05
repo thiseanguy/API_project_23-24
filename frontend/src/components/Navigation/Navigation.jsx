146163c0bcae816a4d7f094ddf6fb0084bb4390f
Sean Hatcher <thiseanguy@gmail.com>
2024-06-04 22:55:32 -0500

    implemented login modal

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,6 +1,8 @@
 import { NavLink } from 'react-router-dom';
 import { useSelector} from 'react-redux';
 import ProfileButton from './ProfileButton';
+import OpenModalButton from '../OpenModalButton';
+import LoginFormModal from '../LoginFormModal';
 import './Navigation.css';
 
 
@@ -15,7 +17,10 @@ function Navigation({ isLoaded }) {
   ) : (
     <>
       <li>
-        <NavLink to="/login">Log In</NavLink>
+        <OpenModalButton
+          buttonText="Log In"
+          modalComponent={<LoginFormModal />}
+        />
       </li>
       <li>
         <NavLink to="/signup">Sign Up</NavLink>
