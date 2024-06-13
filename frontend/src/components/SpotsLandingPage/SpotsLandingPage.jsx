c6c8cdc5be82af14b851265877225ea7c39a1edc
Sean Hatcher <thiseanguy@gmail.com>
2024-06-12 21:37:35 -0500

    adjusted spot tile layout first spec

--- frontend/src/components/SpotsLandingPage/SpotsLandingPage.jsx
+++ frontend/src/components/SpotsLandingPage/SpotsLandingPage.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchSpots } from '../../store/spots'
 import { NavLink } from 'react-router-dom';
 import { Tooltip } from 'react-tooltip'
+import { IoStar } from "react-icons/io5";
 import './SpotsLandingPage.css';
 
 const SpotsLandingPage = () => {
@@ -29,12 +30,15 @@ const SpotsLandingPage = () => {
               <NavLink to={`/spots/${spot.id}`} className="spot-link">
                 <img src={spot.previewImage} alt={spot.name} className="spot-thumbnail" />
                 <div className="spot-info">
-                  <p>{spot.city}, {spot.state}</p>
-                  <p>
-                    {spot.avgRating !== undefined && spot.avgRating !== null
-                    ? `Avg Rating: ${spot.avgRating.toFixed(1)}`
-                  : 'New'}
-                  </p>
+                  <div className="spot-location-rating">
+                    <p id="city-state">{spot.city}, {spot.state}</p>
+                    <p id="avg-rating">
+                    <IoStar className="star-icon"/>
+                      {spot.avgRating !== undefined && spot.avgRating !== null
+                      ? ` ${spot.avgRating.toFixed(1)}`
+                      : 'New'}
+                    </p>
+                  </div>
                   <p>{`$${spot.price}/ night`}</p>
                 </div>
                 <Tooltip id={`tooltip-spot-tile`} />
