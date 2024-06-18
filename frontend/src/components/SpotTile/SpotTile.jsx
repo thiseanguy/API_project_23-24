d9730795202508588984ab6eb401521b5f697ae2
Sean Hatcher <thiseanguy@gmail.com>
2024-06-17 21:55:54 -0500

    possible fix for deployment

--- frontend/src/components/SpotTile/SpotTile.jsx
+++ frontend/src/components/SpotTile/SpotTile.jsx
@@ -9,26 +9,9 @@ const SpotTile = ({ spot }) => {
   const { setModalContent } = useModal();
 
   const openDeleteModal = () => {
+    // e.stopPropagation();
     setModalContent(<ConfirmDeleteModal spotId={spot.id} closeModal={() => setModalContent(null)} />);
   };
-
-//   return (
-//     <div className="spot-tile">
-//       <NavLink to={`/spots/${spot.id}`}>
-//         <img src={spot.previewImage} alt={spot.name} className="spot-thumbnail" />
-//         <div className="spot-info">
-//           <p>{`${spot.city}, ${spot.state}, ${spot.country}`}</p>
-//           <p>Rating: {spot.avgRating ? spot.avgRating.toFixed(1) : 'New'}</p>
-//           <p>Price: ${spot.price}</p>
-//         </div>
-//       </NavLink>
-//       <li>
-//         <button onClick={openDeleteModal}>Delete</button>
-//         <NavLink to={`/update-spot/${spot.id}`} className="button-like-link">Manage Spots</NavLink>
-//       </li>
-//     </div>
-//   );
-// };
 
 return (
   <div className="spot-tile">
